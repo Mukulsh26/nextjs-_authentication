531483686c1cea8e5ccb35a6da116f70d3c24447
Mukulsh26 <mukulsh7496@gmail.com>
2024-12-29 23:27:22 +0530

    feat: fixed changes

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,14 @@
+export default function PrivacyPolicy() {
+    return (
+      <div style={{ padding: "20px", fontFamily: "Arial" }}>
+        <h1>Privacy Policy</h1>
+        <p>
+          We respect your privacy and are committed to protecting your data.
+          This Privacy Policy explains how we collect, use, and protect your
+          personal information.
+        </p>
+        <p>Contact us at mukulsh7496@gmail.com if you have any questions.</p>
+      </div>
+    );
+  }
+  
